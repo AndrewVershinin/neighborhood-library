68d49b72b750cec8150d546a1549d9469062affe
Anrew <andrvolg.job@gmail.com>
2025-04-18 15:40:49 -0400

    Library app is ready

--- NeighborhoodLibrary/src/main/java/com/pluralsight/LibraryApp.java
+++ NeighborhoodLibrary/src/main/java/com/pluralsight/LibraryApp.java
@@ -7,14 +7,15 @@ public class LibraryApp {
 
         Scanner input = new Scanner(System.in);
 
-        Book[] inventory = new Book[20];
+        Book[] inventory = new Book[21];
 
-        for (int i = 0; i < inventory.length; i++) {
+        for (int i = 0; i < 20; i++) {
             inventory[i] = new Book(i +1, "100-0" + (i + 1), "Book " + (i + 1));
         }
 
-//        showAvailableBooks(inventory);
-//        showCheckedOutBooks(inventory);
+        Book extraBook = new Book(21, "100-021", "Graf Monte-Cristo");
+        extraBook.checkOut("Andrei");
+        inventory[20] = extraBook;
 
         boolean userRunning = true;
 
@@ -29,10 +30,10 @@ public class LibraryApp {
 
             switch (useChoice) {
                 case 1:
-                    showAvailableBooks(inventory);
+                    showAvailableBooks(inventory, input);
                     break;
                 case 2:
-                    showCheckedOutBooks(inventory);
+                    showCheckedOutBooks(inventory, input);
                     break;
                 case 0:
                     userRunning = false;
@@ -44,20 +45,42 @@ public class LibraryApp {
         }
         input.close();
     }
-    public static void showAvailableBooks(Book[] inventory) {
-        System.out.println("Available Books:");
+    public static void showAvailableBooks(Book[] inventory, Scanner input) {
+        System.out.println("\nAvailable Books:");
+        boolean anyAvailable = false;
+
         for (int i = 0; i < inventory.length; i++) {
             Book book = inventory[i];
             if (!book.isCheckedOut()) {
                 System.out.println("ID: " + book.getId() +
                         ", Title: " + book.getTitle() +
                         ", ISBN: " + book.getIsbn());
+                anyAvailable = true;
+            }
+        }
+
+        if (!anyAvailable) {
+            System.out.println("All books are checked out.");
+            return;
+        }
+
+        System.out.print("\nEnter the ID of the book to check out (or 0 to go back): ");
+        int id = input.nextInt();
+        input.nextLine(); // clear newline
+
+        if (id > 0 && id <= inventory.length) {
+            Book book = inventory[id - 1];
+            if (!book.isCheckedOut()) {
+                System.out.print("Enter your name: ");
+                String name = input.nextLine();
+                book.checkOut(name);
+            } else {
+                System.out.println("That book is already checked out.");
             }
         }
     }
-    public static void showCheckedOutBooks(Book[] inventory) {
+    public static void showCheckedOutBooks(Book[] inventory, Scanner input) {
         System.out.println("\nChecked Out Books:");
-
         boolean anyCheckedOut = false;
 
         for (Book book : inventory) {
@@ -69,8 +92,23 @@ public class LibraryApp {
                 anyCheckedOut = true;
             }
         }
+
         if (!anyCheckedOut) {
             System.out.println("Every book is available.\n");
+            return;
+        }
+
+        System.out.print("\nEnter the ID of the book to check in (or 0 to go back): ");
+        int id = input.nextInt();
+        input.nextLine();
+
+        if (id > 0 && id <= inventory.length) {
+            Book book = inventory[id - 1];
+            if (book.isCheckedOut()) {
+                book.checkIn();
+            } else {
+                System.out.println("That book is not currently checked out.");
+            }
         }
     }
 
